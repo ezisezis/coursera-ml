b056821b9d8c5173f396c8b224c0d653901e4bcc
Eduards Klavins <azyrock1992@gmail.com>
2018-02-27 14:46:02 +0100

    ex1 done, ex2 added

--- ex1/ex1/gradientDescent.m
+++ ex1/ex1/gradientDescent.m
@@ -16,14 +16,9 @@ for iter = 1:num_iters
     % Hint: While debugging, it can be useful to print out the values
     %       of the cost function (computeCost) and gradient here.
     %
-
-
-
-
-
-
-
     % ============================================================
+    deriv_J = X'*(X*theta - y);
+    theta = theta - alpha * (1/m) * deriv_J;
 
     % Save the cost J in every iteration    
     J_history(iter) = computeCost(X, y, theta);
